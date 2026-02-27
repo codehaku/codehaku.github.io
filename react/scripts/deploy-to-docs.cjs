6530866fd31aa6c5ec13b7e35b249abbf28550c7
codelahiru <gayan.hatharasinghe@gmail.com>
2026-02-27 20:51:50 +0530

    new chganges

---
+++ react/scripts/deploy-to-docs.cjs
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = path.resolve(__dirname, "..");
+const buildDir = path.join(rootDir, "build");
+const docsDir = path.join(rootDir, "..", "docs");
+
+if (!fs.existsSync(buildDir)) {
+  console.error("Build directory does not exist. Run `npm run build` first.");
+  process.exit(1);
+}
+
+fs.rmSync(docsDir, { recursive: true, force: true });
+
+function copyDir(src, dest) {
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest, { recursive: true });
+  }
+
+  for (const entry of fs.readdirSync(src, { withFileTypes: true })) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+
+    if (entry.isDirectory()) {
+      copyDir(srcPath, destPath);
+    } else if (entry.isFile()) {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
+}
+
+copyDir(buildDir, docsDir);
+console.log("React build copied to ../docs");
+
